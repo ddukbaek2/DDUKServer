4566ff7a2b2c3286c2b55158d7ccc75cdef4b767
ddukbaek2 <dagraac@naver.com>
2024-01-22 02:10:20 +0900

    work

--- HTTPBackendServerExample/Scripts/ExampleSession.cs
+++ HTTPBackendServerExample/Scripts/ExampleSession.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using DDUKServer.HTML;
+using System.Net;
 using System.Threading.Tasks;
 
 
@@ -12,6 +13,39 @@ namespace DDUKServer.HTTPBackendServerExample
 
 		protected override Task<HttpStatusCode> OnProcessRequest(HttpListenerRequest request, HttpListenerResponse response)
 		{
+			var root = new Division
+			{
+				Children =
+				{
+					new Division
+					{
+					},
+					new Division
+					{
+						Children =
+						{
+							new Paragraph
+							{
+								Attributes =
+								{
+									{ "background-color", "#FF0000" },
+								},
+
+								Value = "Server Side Rendering",
+							},
+
+							//new Image
+							//{
+							//	Attributes =
+							//	{
+							//		{ "src", "Assets/zzio.png" },
+							//	},
+							//},
+						},
+					}
+				},
+			};
+
 			return base.OnProcessRequest(request, response);
 		}
 	}
